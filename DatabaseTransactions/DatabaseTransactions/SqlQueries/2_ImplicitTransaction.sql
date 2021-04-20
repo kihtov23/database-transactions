f7fc44397148d65771377571ed7f58f6c3d5b9dc
Oleg Kikhtov <kihtov23@gmail.com>
2021-04-20 17:12:21 +0300

    Add more sql

--- DatabaseTransactions/DatabaseTransactions/SqlQueries/2_ImplicitTransaction.sql
+++ DatabaseTransactions/DatabaseTransactions/SqlQueries/2_ImplicitTransaction.sql
@@ -1,2 +1,15 @@
 ﻿USE FishingLog;
 
+SET IMPLICIT_TRANSACTIONS ON;
+
+UPDATE Accounts
+  SET 
+      [Name] = 'qwerty'
+WHERE Id = 1;
+
+UPDATE Accounts
+  SET 
+      [Name] = '54321'
+WHERE Id = 2;
+
+ROLLBACK;
--- DatabaseTransactions/DatabaseTransactions/SqlQueries/3_ExplicitTransaction.sql
+++ DatabaseTransactions/DatabaseTransactions/SqlQueries/3_ExplicitTransaction.sql
@@ -1,2 +1,16 @@
 ﻿USE FishingLog;
 
+SET IMPLICIT_TRANSACTIONS OFF;
+
+BEGIN TRANSACTION;
+UPDATE Accounts
+  SET 
+      [Name] = 'Test1234'
+WHERE Id = 1;
+
+UPDATE Accounts
+  SET 
+      [Name] = 'Test12345'
+WHERE Id = 2;
+
+ROLLBACK;
